d2fd119371402133cb24f35c20ea27c11e33b80a
James Gannon <S00196554@mail.itsligo.ie>
2021-05-11 09:52:47 +0100

    Added Unit Testing

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using JamesGannon_S00196554;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //ARRANGE
+            Game game1 = new Game();
+            game1.Price = 100;
+            decimal finalPrice = 50;
+            //ACT
+            game1.DecreasePrice(50);
+            //ASSERT
+            Assert.AreEqual(finalPrice, game1.Price);
+        }
+    }
+}
