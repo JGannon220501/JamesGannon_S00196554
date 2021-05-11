f066dc09cd0948d11d94c69f69beff0f28d8ed36
James Gannon <S00196554@mail.itsligo.ie>
2021-05-11 09:43:29 +0100

    Created class with Properties

---
+++ JamesGannon_S00196554/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JamesGannon_S00196554
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public decimal MetacriticScore { get; set; }
+        public string Description { get; set; }
+        public string Platform { get; set; }
+        public decimal Price { get; set; }
+        public string GameImage { get; set; }
+
+
+    }
+}
