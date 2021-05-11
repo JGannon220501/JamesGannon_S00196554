db3b0d6b384cd70569ec2ae5632b42ea08750c99
James Gannon <S00196554@mail.itsligo.ie>
2021-05-11 11:57:29 +0100

    Created XAML interface

--- JamesGannon_S00196554/MainWindow.xaml.cs
+++ JamesGannon_S00196554/MainWindow.xaml.cs
@@ -20,9 +20,34 @@ namespace JamesGannon_S00196554
     /// </summary>
     public partial class MainWindow : Window
     {
+        GameData db;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            db = new GameData();
+            var query = from g in db.Games
+                        select g.Name;
+            GamesLbx.ItemsSource = query;
+        }
+
+        private void GamesLbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Game selectedGame = GamesLbx.SelectedItem as Game;
+            if(selectedGame != null)
+            {
+                DetailsTblk.Text = $"Name: {selectedGame.Name}," +
+                                   $"Description: {selectedGame.Description}, " +
+                                   $"Platform: {selectedGame.Platform}, " +
+                                   $"MetaCritic Score: {selectedGame.MetacriticScore}, " +
+                                   $"Price: {selectedGame.Price}";
+                image.Source = (ImageSource)(from i in db.Games
+                               select i.GameImage);
+            }
         }
     }
 }
